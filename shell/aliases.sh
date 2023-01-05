a5e095c40dd1d2b5b2ff7ca4a2b3492bbccfae9a
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-01-05 21:12:31 +0800

    Add more aliases for Git and Docker

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,6 +1,8 @@
 alias codei       = 'code-insiders'
 alias {py,python} = 'python3'
 alias c           = 'clear'
+
+# Git stuff
 alias gp          = "git push"
 alias gfp         = "git fetch && git pull"
 alias gs          = "git status"
@@ -8,4 +10,14 @@ alias gaa         = "git add -A"
 alias gcl         = "git clone"
 alias gpu         = "git push --set-upstream"
 alias gc          = "git commit"
-alias c           = "clear"
+alias grc         = "git rm -r --cached ."
+alias gsync       = "grc; gaa"
+
+# Docker stuff
+alias dp          = "docker ps"
+alias dpl         = "docker pull"
+alias dps         = "docker push"
+alias db          = "docker build"
+alias dr          = "docker run"
+alias dcu         = "docker-compose up"
+alias dcd         = "docker-compose down"
