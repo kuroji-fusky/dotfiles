a7ccaf58eadb8431116d8900e7fbd93da3d46d97
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-02-06 20:45:07 +0800

    Turn into UserScript

--- browser-scripts/dontRecommendThisShortPlease.js
+++ browser-scripts/dontRecommendThisShortPlease.js
@@ -1,3 +1,10 @@
+// ==UserScript==
+// @name         Don't Recommend This Short
+// @description  A simple script that will press a button to never recommend a brain-dead short ever again!
+// @author       Kuroji Fusky
+// @match        https://www.youtube.com/shorts/*
+// @grant        none
+// ==/UserScript==
 (window.onkeydown = (e) => {
 	// TODO: Disable action if the URL isn't under 'youtube.com/shorts'
 	if(e.key=='n') $$('ytd-menu-service-item-renderer')[1].click()
