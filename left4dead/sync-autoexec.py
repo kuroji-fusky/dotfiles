de71b01b2fab5f131d80d3b26fe155fd4337bf29
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-02-25 08:03:19 +0800

    narrow the search only for `autoexec.cfg`

--- left4dead/sync-autoexec.py
+++ left4dead/sync-autoexec.py
@@ -5,7 +5,7 @@ import os.path
 def main():
     steam_path = "D:\\SteamLibrary\\steamapps\\common"
     for dirpath, _, filenames in os.walk(steam_path):
-        for filename in [f for f in filenames if f.endswith(".cfg")]:
+        for filename in [f for f in filenames if f.endswith("autoexec.cfg")]:
             print(os.path.join(dirpath, filename))
 
 
