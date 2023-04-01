fb15768045284cdac67461c40180f8ae1094f5ec
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-04-01 23:44:22 +0800

    so some cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,10 @@
 read -p "Email plis: " email
 read -p "Your name b0ss: " name
 
-ssh-keygen -t ed25519 -C "$email" -N "" -f ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub
-
 git config --global user.email "$email"
 git config --global user.name "$name"
-git config --global core.ignorecase false
 
 echo "Setting up custom aliases"
-. ./bash/aliases.sh
+. './bash/aliases.sh'
 
 echo "All set bitch"
