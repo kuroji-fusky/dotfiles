32bba7c410972516692b52e9fd21bf835e35a6b5
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-09-26 13:59:26 +0800

    simplify powershell aliases

--- pwsh/aliases.ps1
+++ pwsh/aliases.ps1
@@ -1,24 +1,15 @@
 # =================================================
 # ALIASES
 
-Set-Alias -Name codei -Value 'code-insiders'
+Set-Alias -Name cdi -Value 'code-insiders'
 Set-Alias -Name c -Value Clear-Host
 
 # =================================================
 # Navigate directories
 
-function Set-LocationHome { & Set-Location ~ }
-Set-Alias -Name "~" -Value Set-LocationHome
+function .. { & Set-Location .. }
+function ... { & Set-Location ../.. }
 
-function Set-LocationUp { & Set-Location .. }
-Set-Alias -Name "cd.." -Value Set-LocationUp
-Set-Alias -Name ".." -Value Set-LocationUp
-
-function Set-LocationUp2 { & Set-Location ../.. }
-Set-Alias -Name "..." -Value Set-LocationUp2
-
-function Set-GithubDir { & Set-Location ~/GitHub }
-Set-Alias -Name kgh -Value Set-GithubDir
 
 # =================================================
 # Register `mkcd` command for Windows
@@ -31,8 +22,6 @@ function mkcd {
 
   Set-Location $args[0] -PassThru
 }
-
-Set-Alias -Name mkcd -Value mkcd
 
 # =================================================
 # Restart dat shit
@@ -47,14 +36,12 @@ function ToggleTheme {
   $regDir = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
   $appKey = "AppsUseLightTheme"
   $sysKey = "SystemUsesLightTheme"
+
   $currentValue = Get-ItemProperty -Path $regDir -Name $appKey | Select-Object -exp $appKey
   $newValue = !$currentValue + 0
 
   Set-ItemProperty -Path $regDir -Name $appKey -Value $newValue -Type Dword -Force
   Set-ItemProperty -Path $regDir -Name $sysKey -Value $newValue -Type Dword -Force
-
-  taskkill.exe -f -im explorer.exe
-  Start-Process explorer
 }
 
 Set-Alias -Name tt -Value ToggleTheme
@@ -62,59 +49,28 @@ Set-Alias -Name tt -Value ToggleTheme
 # =================================================
 # Add git aliases to make commiting code on the command line easier
 
-function GitAddAll { & git add --all }
-Set-Alias -Name ga -Value GitAddAll -Force -Option AllScope
-
-function GitBranch { & git branch }
-Set-Alias -Name gb -Value GitBranch -Force -Option AllScope
-
-function GitCommit { & git commit -m }
-Set-Alias -Name gc -Value GitCommit -Force -Option AllScope
-
-function GitClone { & git clone }
-Set-Alias -Name gcl -Value GitClone -Force -Option AllScope
-
-function GitCheckout { & git checkout }
-Set-Alias -Name gch -Value GitCheckout -Force -Option AllScope
-
-function GitCheckoutOrphan { & git checkout --orphan }
-Set-Alias -Name gcho -Value GitCheckoutOrphan -Force -Option AllScope
-
-function GitFetch { & git fetch --verbose }
-Set-Alias -Name gf -Value GitFetch -Force -Option AllScope
-
-function GitFetchAll { & git fetch --all --verbose }
-Set-Alias -Name gfa -Value GitFetchAll -Force -Option AllScope
-
-function GitPull { & git pull }
-Set-Alias -Name gl -Value GitPull -Force -Option AllScope
-
-function GitPush { & git push }
-Set-Alias -Name gp -Value GitPush -Force -Option AllScope
-
-function GitPushUpstream { & git push --set-upstream }
-Set-Alias -Name gpu -Value GitPushUpstream -Force -Option AllScope
-
-function GitRefLog { & git reflog . }
-Set-Alias -Name grf -Value GitRefLog
-
-function GitStatus { & git status -sb }
-Set-Alias -Name gs -Value GitStatus -Force -Option AllScope
-
-function GitStashUnstaged { & git stash -u }
-Set-Alias -Name gsu -Value GitStashUnstaged
-
-function GitStashApply { & git stash apply }
-Set-Alias -Name gsa -Value GitStashApply
-
-function GitRmCache { & git rm -r --cached . }
-function GitResetCache { & GitRmCache && GitAddAll }
-Set-Alias -Name gra -Value GitResetCache -Force -Option AllScope
+function ga { git add --all }
+function gb { git branch }
+function gc { git commit -m $args[0] --verbose }
+function gcl { git clone -m $args[0] --verbose }
+function gch { git checkout $args[0] }
+function gcho { git checkout --orphan }
+function gf { & git fetch --verbose }
+function gfa { & git fetch --all --verbose }
+function gpl { git pull }
+function gps { git push }
+function gpu { & git push --set-upstream }
+function grf { & git reflog . }
+function gs { & git status -sb }
+function gss { & git stash -u }
+function gsa { & git stash apply }
+function grc { 
+  git rm -r --cached .
+  git add -A --verbose
+}
 
 # =================================================
-# Git functions
-
-# Update branch from both local and origin
+# Git function to update branch name from both local and origin
 function Rename-GitBranch {
   param (
     [string]$oldBranch,
@@ -137,21 +93,11 @@ Set-Alias -Name upb -Value Rename-GitBranch
 # =================================================
 # Common dev commands
 
-function YarnInstall { & yarn install --force }
-Set-Alias -Name yi -Value YarnInstall
+function yi { yarn install }
 
 # Yiff OwO
-function YarnInstallForce { & yarn install --force }
-Set-Alias -Name yif -Value YarnInstallForce
-
-function YarnDev { & yarn dev }
-Set-Alias -Name yd -Value YarnDev
-
-function YarnBuild { & yarn build }
-Set-Alias -Name yb -Value YarnBuild
-
-function YarnStart { & yarn start }
-Set-Alias -Name ys -Value YarnStart
-
-function YarnPreview { & yarn preview }
-Set-Alias -Name yp -Value YarnPreview
+function yif { yarn install --force }
+function yd { yarn dev }
+function yb { yarn build }
+function ys { yarn start }
+function yp { yarn preview }
