59b78bcb8fc091115cb4117ca3b2dc59ef841a6f
Kerby Keith Aquino <skepfoosky15@gmail.com>
2024-02-13 09:59:03 +0800

    improve aliased git commands

--- pwsh/aliases.ps1
+++ pwsh/aliases.ps1
@@ -82,10 +82,28 @@ Remove-Alias -Name gp -Force -ErrorAction SilentlyContinue
 Remove-Alias -Name gc -Force -ErrorAction SilentlyContinue
 
 function gaa { & git add --all }
-function gb { & git branch }
+function gb { 
+  Write-Output ">> Local branches:"
+  git branch
+  Write-Output ">> Remote branches:"
+  git branch --remote
+}
 function gc { & git commit -m $args[0] --verbose }
 function gcn { & git commit -m $args[0] --no-verify --verbose }
-function gcl { & git clone $args[0] --verbose }
+function gcl { 
+  $gitUrl = $args[0].ToString()
+  $repoDir = $gitUrl.Split("/")[-1].Replace(".git", "")
+
+  if (!$gitUrl.StartsWith("https://github.com")) {
+    Write-Error "Not a valid git repo"
+  }
+
+  git clone  $gitUrl --verbose
+
+  Set-Location $repoDir
+
+  git fetch --all --verbose
+}
 function gch { & git checkout $args[0] }
 function gcho { & git checkout --orphan $args[0] }
 function gd { & git diff }
