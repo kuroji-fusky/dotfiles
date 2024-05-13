cf06535a5403cd46685081cc1975316449e890c8
Kerby Keith Aquino <hello@kurojifusky.com>
2024-05-13 12:08:29 +0800

    more organizing

--- pwsh/profile-win.ps1
+++ pwsh/profile-win.ps1
@@ -1,1 +1,7 @@
-oh-my-posh init pwsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/velvet.omp.json" | Invoke-Expression
+Get-ChildItem $env:USERPROFILE\gh\dotfiles\pwsh\aliases | ForEach-Object { . $_.FullName }
+
+if (Get-Command -Name "oh-my-posh.exe" -ErrorAction SilentlyContinue) {
+  oh-my-posh init pwsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/velvet.omp.json" | Invoke-Expression
+} else {
+  Write-Error "Failed to initialize Oh My Posh"
+}
