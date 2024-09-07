61eea8f289782834aee3006e18384b7aacd9bdf2
Kerby Keith Aquino <hello@kurojifusky.com>
2024-09-07 20:35:21 +0800

    add drive size

---
+++ pwsh/drive-size.ps1
@@ -0,0 +1,40 @@
+function Get-RoundedSize {
+  param (
+    [long]$Bytes
+  )
+    
+  $_TBSize = 1TB
+  $_PBSize = 1PB
+
+  $SizeInGB = [math]::Round($Bytes / 1GB, 2)
+  $SizeInTB = [math]::Round($Bytes / $_TBSize, 2)
+  $SizeInPB = [math]::Round($Bytes / $_PBSize, 2)
+
+  if ($Bytes -ge $_PBSize) {
+    return "$SizeInPB PB"
+  }
+  elseif ($Bytes -ge $_TBSize) {
+    return "$SizeInTB TB"
+  }
+  else {
+    return "$SizeInGB GB"
+  }
+}
+
+function Get-DriveSize {
+  $MountedVolumes = Get-Volume | Where-Object { $_.DriveLetter -ne $null }
+
+  $TotalSize = $($MountedVolumes | Measure-Object -Property Size -Sum).Sum
+  $RemainingSize = $($MountedVolumes | Measure-Object -Property SizeRemaining -Sum).Sum
+
+  $RelativeSize = "$([math]::Round($RemainingSize / $TotalSize * 100, 2))%"
+
+  $TotalSizeReadable = Get-RoundedSize -Bytes $TotalSize
+  $RemainingSizeReadable = Get-RoundedSize -Bytes $RemainingSize
+
+  $MountedVolumeTable = $MountedVolumes | Select-Object DriveLetter, FileSystemLabel , Size, SizeRemaining
+
+  $MountedVolumeTable
+
+  Write-Output "`nYou have a total of $TotalSizeReadable of space and $RelativeSize ($RemainingSizeReadable) remaining left"
+}
