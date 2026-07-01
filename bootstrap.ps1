a9598be19d3a2cbb32231edb91382c0de0dbee59
Kuro <hello@kurojifusky.com>
2026-07-02 02:11:34 +0800

    symlink and admin stuf

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,8 +1,7 @@
-#Requires -RunAsAdministrator
-
-$CurrentPSRootDir = "$PSScriptRoot\scripts\pwsh"
-$BootstrapPartialsDir = "$CurrentPSRootDir\bootstrap-partials"
-$SystemEnvPath = $env:USERPROFILE
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+  Write-Output "Requires admin to run you dingus"
+  exit 1
+}
 
 # Thanks to this :3 https://stackoverflow.com/a/29424207/18905871
 function Get-CheckCommand {
@@ -13,18 +12,36 @@ function Get-CheckCommand {
   return [bool](Get-Command -Name $CmdName -ErrorAction SilentlyContinue)
 }
 
+$SystemEnvPath = $env:USERPROFILE
+
+$CurrentPSRootDir = "$PSScriptRoot\scripts\pwsh"
+$ConfigsPath = "$PSScriptRoot\configs\windows"
+$BootstrapPartialsDir = "$CurrentPSRootDir\bootstrap-partials"
+
+$DocumentsPath = "$SystemEnvPath\Documents\PowerShell"
+
 # Stuff that requires admin privilages for screwing around with the registry
 Write-Output "Writing stuff to registry"
 & "$BootstrapPartialsDir\registry.ps1"
 
-$SymLinkLocations = @{
-  # <source> = <target>
-  "$CurrentPSRootDir/.gitconfig" = "$SystemEnvPath/.gitconfig"
+$SymLinkLocations = @(
+  # <link> = <target>
+  @("$CurrentPSRootDir\.gitconfig", "$SystemEnvPath\.gitconfig"),
+  @("$ConfigsPath\Microsoft.PowerShell_profile.ps1", "$DocumentsPath\Microsoft.PowerShell_profile.ps1"),
+  @("$ConfigsPath\powershell.config.json", "$DocumentsPath\powershell.config.json")
+)
+
+foreach ($location in $SymLinkLocations) {
+  $symlinkPath, $target = $location
+
+  Write-Output "Linking $symlinkPath -> $target"
+
+  New-Item -ItemType SymbolicLink -Path $target -Target $symlinkPath -ErrorAction Continue
 }
 
 if (Get-CheckCommand -CmdName "winget") {
   Write-Output "Installing your shit"
-  winget import "$CurrentPSRootDir\winget.json" --verbose
+  winget import "$ConfigsPath\winget.json" --verbose
 }
 else {
   Write-Output "winget not installed"
